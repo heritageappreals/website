f74f3e480c881b6542239575c75667a4f0e9ca96
Dheeraj <heritage.apparels4444@gmail.com>
2025-08-11 08:16:00 -0400

    Some changes in new .php file

--- new.php
+++ new.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Get Best Clothing Menuc</title>
+    <title>Get Best Clothing Menucfering in the Ludhiana | Heritage Appreals</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
